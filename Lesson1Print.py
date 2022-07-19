52454d149c3fe9b2e8998a3f25dd5303ea865f61
Don Overton <donald.overton@asu.edu>
2022-07-18 21:35:20 -0700

    Print section

---
+++ Lesson1Print.py
@@ -0,0 +1,64 @@
+"""
+Created on Monday July 18th 2022
+
+@author doverto1
+
+"""
+from xml.dom import HierarchyRequestErr
+
+
+print ("Hello world!")
+
+print ('Hello world!')
+
+print ("\"Hello world!\".")
+
+print ()
+
+print ("I'm learning Python. \
+And we're starting with the Print Statements.")
+
+print ("""I'm learning Python.
+And we're starting with the print Statements.""")
+
+print ("I'm learning Python. \nAnd we're starting with the Print Statements.")
+
+print (r"I'm learning Python.\nAnd we're starting with the Print Statements.")
+
+print ()
+
+print ("We'll first learn how to Print.")
+print ("Then we'll learn how to comment code.")
+
+print ("We'll first learn how to Print.", end='')
+print ("Then we'll learn ho to comment code.")
+
+print ("We'll first learn how to Print.", end='..')
+print ("Then we'll learn how to comment code.")
+
+#print ("Whisper: can you hear me?")
+print ("Can you hear me now?") #print ("How about now?")
+
+print ("# And how about now? #Can you hear me now?")
+
+print ()
+
+print ("Okay so may be I need to repeat myself..." *3)
+
+print ("Can you hear me now..." + "Can you hear me now..." + "Can you hear me now...")
+
+print ("Number of times I repeated myself:", 3)
+
+print ("Hi", "there", "how", "are", "you")
+
+print ("Hi I have", 20, "oranges and", 30, "apples")
+
+print()
+
+#test
+
+#print (""Hi there!"") #no
+
+# print ("Hi  #no
+#        there")
+print ("I", "have", 32, "students in my class")
