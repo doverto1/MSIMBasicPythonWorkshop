e86caa03e63aa415fcc42fe2ed2a05a08440e5d7
Don Overton <donald.overton@asu.edu>
2022-08-02 15:41:18 -0700

    added lesson2

---
+++ Lesson2Variables.py
@@ -0,0 +1,48 @@
+"""
+Created on July 20th, 2022
+
+@author: doverto1
+"""
+
+print()
+
+distance_traveled_mi = 100
+
+time_taken_min = 1200
+
+time_taken_hr = time_taken_min/60
+
+print ("Number of miles traveled", distance_traveled_mi)
+print ("Number of hours traveled", time_taken_hr)
+print ("Speed in mph", distance_traveled_mi / time_taken_hr)
+
+print()
+
+# test
+#no
+#print ("Time take in minutest", Time_Taken_Min)
+
+#yes
+_total_memory =0
+
+#no
+#_total% = 25
+
+#yes
+totalPercentage = 10
+
+#no
+#1stPass = 3
+
+#yes
+myName = "Hina Arora"
+
+#no
+#class = "CIS 415"
+
+#yes
+fruits = "Apples and Oranges"
+
+numApples = 10
+numOranges = 20
+print ("I have", fruits, ".... I have", numApples, "Apples and ", numOranges, "Oranges")
