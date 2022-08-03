330162c5e1f62babc819f07ee215f1424ddb1067
Don Overton <donald.overton@asu.edu>
2022-08-02 20:53:52 -0700

    added action and part of lesson 3

---
+++ Lesson3NumOps.py
@@ -0,0 +1,47 @@
+"""
+Created on Tuesday August 2nd 15:53
+@author: doverto1
+"""
+
+print("Power Operator: **")
+
+print("2**4 = ", 2**4)
+
+print("-3**2 = ", -3**2)
+print("-(3**2) = ", -(3**2))
+
+print("3**-2 = ", 3**-2)
+
+print()
+
+print("Unary and bitwise operators: -  ~")
+
+print("-2 = ", -2)
+
+print("~2 = ", ~2, "(bitwise inversion)")
+
+print("Floor Operator: 101//2 =", 101//2)
+
+print("Modulus Operator: 101%2 =", 101%2)
+
+print()
+
+print("Shifting operators: >> <<")
+
+print("100 >> 3 = ", 100 >> 3)
+
+print("  same as : 100 // (2**3) = ", 100 // (2**3))
+
+print("100 << 3 = ", 100 << 3)
+
+print()
+
+print( "Binary bitwise operators: | & ^")
+
+print("3 | 0 = ", 3 | 0)
+
+print("3 ^ 3 = ", 3 ^ 3)
+
+print("3 & 0 = ", 3 & 0)
+
+print()
